09e0215085ce93c42fac706976b807d700e09963
Michal Hrusecky <Michal@Hrusecky.net>
2012-11-22 14:39:46 +0100

    Support for fate# and bnc#
    Quite often we use fate#123 and bnc#456 to refer to what we did. This
    commit makes these expressions into links so it is easier to follow.

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,10 @@
 module EntriesHelper
+   def display_entry(text)
+      newtext = h text
+      newtext = auto_link(newtext, :html => { :target => '_blank' })
+      newtext = truncate(newtext, :length => 30, :separator => '/', :omission => '/...' )
+      newtext = newtext.gsub(/bnc#([0-9]+)/, '<a href="https://bugzilla.novell.com/show_bug.cgi?id=\1" target="_blank">bnc#\1</a>')
+      newtext = newtext.gsub(/fate#([0-9]+)/, '<a href="https://fate.suse.com/\1" target="_blank">fate#\1</a>')
+      newtext.html_safe
+   end
 end
