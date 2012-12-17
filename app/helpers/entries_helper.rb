67d837957f522dddf35c3e14c519965884a0e9fc
Michal Hrusecky <Michal@Hrusecky.net>
2012-12-17 13:49:16 +0100

    Drop days references, get rid of link_to
    Due to performance issues dropping date references and replacing link_to
    with static text.

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -7,47 +7,13 @@ module EntriesHelper
          text = truncate(i, :length => 30, :separator => '/', :omission => '/...' )
       end
 
-      # Make bnc# and fate# work
-      text = text.gsub(/bnc#([0-9]+)/,
-               link_to('bnc#\1', 
-                       'https://bugzilla.novell.com/show_bug.cgi?id=\1',
-                       html_options = { :target => '_blank' }) )
-      text = text.gsub(/fate#([0-9]+)/,
-               link_to('fate#\1',
-                       'https://fate.suse.com/\1',
-                       html_options = { :target => '_blank' }) )
-
-      # Make day references work
-      text = text.gsub(/(#monday)/i,    link_to('\1', '#' + (
-               entry.created_at.to_date - 
-               (1 + entry.created_at.to_date.days_to_week_start(:tuesday)).days
-               ).to_s(:db)))
-      text = text.gsub(/(#tuesday)/i,   link_to('\1', '#' + (
-               entry.created_at.to_date - 
-               (1 + entry.created_at.to_date.days_to_week_start(:wednesday)).days
-               ).to_s(:db)))
-      text = text.gsub(/(#wednesday)/i, link_to('\1', '#' + (
-               entry.created_at.to_date - 
-               (1 + entry.created_at.to_date.days_to_week_start(:thursday)).days
-               ).to_s(:db)))
-      text = text.gsub(/(#thursday)/i,  link_to('\1', '#' + (
-               entry.created_at.to_date - 
-               (1 + entry.created_at.to_date.days_to_week_start(:friday)).days
-               ).to_s(:db)))
-      text = text.gsub(/(#friday)/i,    link_to('\1', '#' + (
-               entry.created_at.to_date - 
-               (1 + entry.created_at.to_date.days_to_week_start(:saturday)).days
-               ).to_s(:db)))
-      text = text.gsub(/(#saturday)/i,  link_to('\1', '#' + (
-               entry.created_at.to_date - 
-               (1 + entry.created_at.to_date.days_to_week_start(:sunday)).days
-               ).to_s(:db)))
-      text = text.gsub(/(#sunday)/i,    link_to('\1', '#' + (
-               entry.created_at.to_date - 
-               (1 + entry.created_at.to_date.days_to_week_start(:monday)).days
-               ).to_s(:db)))
-      text = text.gsub(/(#yesterday)/i, link_to('\1', '#' + (
-               entry.created_at.to_date - 1.day).to_s(:db)))
+      if /#/ =~ text
+         # Make bnc# and fate# work
+         text = text.gsub(/bnc#([0-9]+)/,
+                          '<a href="https://bugzilla.novell.com/show_bug.cgi?id=\1" target="_blank">bnc#\1</a>')
+         text = text.gsub(/fate#([0-9]+)/,
+                          '<a href="https://fate.suse.com/\1" target="_blank">bnc#\1</a>')
+      end
 
       # Render it
       text.html_safe
