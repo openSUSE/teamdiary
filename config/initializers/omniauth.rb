e2b12353b19038fdf5253ef8b1cc7fb37a3f6ea0
Christopher Hofmann <cwh@webeve.de>
2012-12-18 15:38:56 +0100

    Don't use global temp dir

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     require 'openid/store/filesystem'
-    provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
+    provider :openid, :store => OpenID::Store::Filesystem.new(Rails.root.join('tmp').to_s)
 end
